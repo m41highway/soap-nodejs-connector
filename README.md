b5dbee9d716c84c0a9453e7e4244b0e5194d45f4
Anthony Ng <anthony.ng.ags@gmail.com>
2017-06-21 10:58:10 +0800

    Add create session connection

---
+++ README.md
@@ -0,0 +1,7 @@
+# SOAP API Node Js connector
+This project demotrates how to connect to the Mystifly OnePoint SOAP API server with Node Js
+
+## End points
+### Create Session
+
+... WIP
---
+++ index.js
@@ -0,0 +1,29 @@
+const soap = require('soap');
+const config = require('./config.json');
+
+const url = 'http://onepointdemo.myfarebox.com/V2/OnePoint.svc?wsdl';
+
+soap.createClient(url, function(err, client) {
+
+    // -----------------------------------------------
+    // View all the methods
+    // -----------------------------------------------
+    // console.log(client.describe());
+
+    // -----------------------------------------------
+    // View the specific method
+    // -----------------------------------------------
+    // console.log(client.describe().OnePoint.BasicHttpBinding_IOnePoint.CreateSession);
+
+    client.CreateSession(config, function(err, result) {
+        if (err) console.log(err);
+
+        if (result.CreateSessionResult.Errors) {
+            console.log('Error occurs from SOAP server');
+            console.log(result.CreateSessionResult.Errors);
+        }
+
+        console.log('Success');
+        console.log(result.CreateSessionResult);
+    });
+});
